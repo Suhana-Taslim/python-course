045a528dd85d24c82605a449238f955fdeb35152
Suhana-Taslim <suhana27.shaik@gmail.com>
2024-12-28 23:21:53 +0530

    operators

---
+++ lesson 2/operators.py
@@ -0,0 +1,18 @@
+num1 = 15
+num2 = 2
+
+print("addition:",num1+num2)
+print("subraction:",num1-num2)
+print("multiplication:",num1*num2)
+print("division:",num1/num2)
+print("modulus:",num1%num2)
+print("floor division:",num1//num2)
+print("exponential:",num1**num2)
+print("Square root:",num1**0.5)
+
+print("equal check:",num1==num2)
+print("not equal to:",num1!=num2)
+print("greater than:",num1>num2)
+print("less than:",num1<num2)
+print("greater than or equal to:",num1>=num2)
+print("less than or equal to:",num1<=num2)
