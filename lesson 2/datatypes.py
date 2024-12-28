3d3fccd908bfba88e640f5ccb0d56589da5904ea
Suhana-Taslim <suhana27.shaik@gmail.com>
2024-12-28 23:05:11 +0530

    datatypes

---
+++ lesson 2/datatypes.py
@@ -0,0 +1,17 @@
+name="Suhana Taslim"
+age=15
+height=161.5
+is_student= True
+
+print("The name is:",name)
+print("the data type of name is:" ,type(name))
+print("age is:",age)
+print("th data type of age is:",type(age))
+print("height is:",height)
+print("the data type of height is",type(height))
+print("Is the person a student",is_student)
+print("the data type is:",type(is_student))
+
+print("after typecasting")
+height = int(height)
+print("the data type of height is:",type(height))
