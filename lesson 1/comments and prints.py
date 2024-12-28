1ca0cc806f05988f79b752c2fbfcd1c23733cac3
Suhana-Taslim <suhana27.shaik@gmail.com>
2024-12-28 23:04:58 +0530

    datatypes

---
+++ lesson 1/comments and prints.py
@@ -0,0 +1,11 @@
+#printing greetings
+print("Hello People")
+
+#printing introduction
+print("I have just learnt to use the python code")
+
+#printing numbers
+print(4,5,23,59)
+
+#printing words and numbers
+print("My cat is ",2,"years old")
