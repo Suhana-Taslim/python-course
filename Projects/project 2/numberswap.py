f897eec9471d521d3f164c381fec2b6b14a91ed8
Suhana-Taslim <suhana27.shaik@gmail.com>
2024-12-29 22:38:29 +0530

    swap

---
+++ Projects/project 2/numberswap.py
@@ -0,0 +1,13 @@
+x=input("enter value of x:")
+y=input("enter value of y:")
+z=input("enter value of z:")
+
+temp=x
+x=z
+z=y
+y=temp
+
+print("after sorting")
+print("X value is:",x)
+print("Y value is:",y)
+print("Z value is:",z)
