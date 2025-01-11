01fbbb563af7db9c4db41a7bd12a2a251df5ae5f
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-01-11 22:50:07 +0530

    table

---
+++ Lessons/lesson 5/table.py
@@ -0,0 +1,10 @@
+num = 23
+print("Table of 23")
+for i in range(1,11):
+    mul = num*i
+    print("23 x %d = %d" %(i,mul))
+num = 58
+print("Table of 58")
+for i in range(1,11):
+    mul = num*i
+    print("58 x %d = %d" %(i,mul))
