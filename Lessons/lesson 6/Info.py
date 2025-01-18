670927593a560c0ef52f536f63bc948a87c4aa17
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-01-18 22:57:23 +0530

    Details

---
+++ Lessons/lesson 6/Info.py
@@ -0,0 +1,10 @@
+def details(name):
+    print("Hello Everyone! Myself ^_^",name)
+
+user_name = input("Enter your Name ")
+user_age = input("Enter Your Age : ")
+details(user_name)
+def data(age):
+    print("My age is : ",age)
+
+data(user_age)
