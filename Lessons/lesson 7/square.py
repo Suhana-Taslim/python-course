baf27b546e1eee26b3831de077a482bebaa69f6b
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-01-21 22:49:05 +0530

    square

---
+++ Lessons/lesson 7/square.py
@@ -0,0 +1,16 @@
+import turtle
+
+#Creating Canvas
+turtle.Screen().bgcolor("Green")
+
+sc = turtle.Screen()
+sc.setup(400 ,300)
+
+turtle.title("Welcome to Turlte window")
+
+board = turtle.Turtle()
+
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i = i + 1
