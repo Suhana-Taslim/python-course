f9bd3c591e2fc2c9385f805dab36d7c50032427e
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-01-28 23:15:20 +0530

    dictionary

---
+++ Lessons/lesson 8/dictionary.py
@@ -0,0 +1,30 @@
+my_dictionary = {}
+
+my_dictionary = {1: 'apple', 2: 'ball'}
+
+my_dictionary = {'name': 'Suhana', 1: [2, 4, 3]}
+
+my_dictionary = {'name': 'Taslim', 'age': 16}
+
+print(my_dictionary['name'])
+print(my_dictionary.get('age'))
+
+my_dictionary['age'] = 15
+print(my_dictionary)
+
+my_dictionary['address'] = 'Guntur'
+print(my_dictionary)
+
+my_dictionary.pop('age')
+print(my_dictionary)
+
+print("adress : ",my_dictionary.get('adress'))
+
+the_dic = {}
+the_dic = {'name': 'Afrin', 'age':15, 1:[6, 2, 5]}
+
+print(the_dic['name'])
+print(the_dic.get('age'))
+
+the_dic['adress']= 'HYD'
+print(the_dic)
