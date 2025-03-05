cb58c197997665403bf7e8ab312a994142e4cdf9
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-03-05 20:14:56 +0530

    Modes

---
+++ Projects/project 15/modes.py
@@ -0,0 +1,12 @@
+file = open('Project.txt','r')
+print(file.read())
+file.close()
+
+file = open('Project.txt','r')
+print("\n Read in parts\n")
+print(file.read(8))
+file.close()
+
+file = open('Project.txt','a')
+file.write("  Hi I am Sheru and I am the elder brother of momo.")
+file.close()
