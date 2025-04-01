38e9306c077dacd218726ffae8a86b7274018293
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-04-01 23:01:05 +0530

    tothe power

---
+++ Lessons/lesson 22/power.py
@@ -0,0 +1,16 @@
+def test(n):
+    iteration = 0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end="")
+            iteration+=1
+        print("")
+    print("\nwhen n is",n,"iteration =",iteration)
+test(5)
+test(4)
+test(3)
+test(13)
+test(16)
+
+print("\n with every 'n' the taken = n^2")
+print("(n^2)")
