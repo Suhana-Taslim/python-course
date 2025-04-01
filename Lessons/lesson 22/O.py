16d36290be51d2ab03f486792e58e0a27f454811
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-04-01 22:47:57 +0530

    literation

---
+++ Lessons/lesson 22/O.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    literation = 0
+    print("The total number entered by user is",n)
+    literation+=1
+    print("The total number entered by user is",n)
+    literation+=1
+    print("total literation done by computer is",literation,"\n")
+printnumber(10)
+printnumber(200)
+print("\n with any 'n' the time taken by the computer will not change")
