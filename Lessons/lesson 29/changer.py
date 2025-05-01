3ac53aa9c80701c1a8e96a094c420deaace7ad96
Suhana-Taslim <suhana27.shaik@gmail.com>
2025-05-01 23:02:07 +0530

    coverter

---
+++ Lessons/lesson 29/changer.py
@@ -0,0 +1,11 @@
+number = int(input("enter a number:"))
+binary = bin(number)[2:]
+print("Binary is:",binary)
+
+n = int(input("which bit number do you want to print? (1= first from left); "))
+
+if 1 <= n <=len(binary):
+    Bit binary[n-1]
+    print("Thats a bit.\n",bit)
+else:
+    print("not a bit")
